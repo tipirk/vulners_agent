f884319d5cec18f8088b6696e1c0e9241fc7faf5
Dmitriy Lyutov <lyutov.dmitriy@gmail.com>
2017-11-06 13:13:22 +0300

    get_packages func was changed

--- packages.py
+++ packages.py
@@ -1,37 +1,52 @@
 from pprint import pprint
+from requests import post
+from distro import linux_distribution
 import subprocess
-import requests
 import json
-import distro
 
 
 def get_vuln(data_dict):
     query = json.dumps(data_dict)
     url = 'https://vulners.com/api/v3/audit/audit/'
-    response = requests.post(url, data=query).json()
+    response = post(url, data=query).json()
     return response
 
 
-def get_package_list():
-    package_list_str = subprocess.run(['rpm', '-qa'],
-                                      stdout=subprocess.PIPE).stdout
-    package_list = package_list_str.decode().split('\n')
-    del package_list[-1]
+def get_package_list(os_name):
+    rpm_distros = ('rhel', 'redhat', 'suse', 'centos', 'fedora')
+    dpkg_distros = ('debian', 'ubuntu')
+    if os_name in rpm_distros:
+        package_list_str = subprocess.run(['rpm', '-qa'],
+                                          stdout=subprocess.PIPE).stdout
+        package_list = package_list_str.decode().split('\n')
+        del package_list[-1]
+    elif os_name in dpkg_distros:
+        package_list_str = subprocess.run(['dpkg-query', '-W',
+                                           '-f="${Package} ${Version} '
+                                           '${Architecture}\n"'],
+                                          stdout=subprocess.PIPE).stdout
+        package_list_raw = package_list_str.decode().split('"')
+        package_list = []
+        for i in range(len(package_list_raw) - 1):
+            if package_list_raw[i] != '':
+                package_list.append(package_list_raw[i])
+    else:
+        package_list = []
     return package_list
 
 
 def get_os_info():
-    os_name = distro.linux_distribution()[0]
-    os_version = distro.linux_distribution()[1]
+    os_name = linux_distribution()[0]
+    os_version = linux_distribution()[1]
     return os_name, os_version
 
 
-# package_list = get_package_list()
+package_list = get_package_list()
 os_name = get_os_info()[0]
 os_version = get_os_info()[1]
-# data_dict = {'os': os_name, 'version': os_version, 'package': package_list}
+data_dict = {'os': os_name, 'version': os_version, 'package': package_list}
 
-# vulners_info = get_vuln(data_dict)
-# pprint(vulners_info['data']['packages'])
+vulners_info = get_vuln(data_dict)
+pprint(vulners_info['data']['packages'])
 print(os_name)
 print(os_version)
